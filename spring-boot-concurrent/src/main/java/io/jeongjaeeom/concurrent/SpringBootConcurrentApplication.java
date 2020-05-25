ee39828ecbb7e15a40c9556665390672b87623be
jeongjaeeom <jeongjae.eom@gmail.com>
2020-05-25 14:16:25 +0900

    Add spring-boot-concurrent project

---
+++ spring-boot-concurrent/src/main/java/io/jeongjaeeom/concurrent/SpringBootConcurrentApplication.java
@@ -0,0 +1,13 @@
+package io.jeongjaeeom.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConcurrentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootConcurrentApplication.class, args);
+  }
+
+}
---
+++ spring-boot-concurrent/src/test/java/io/jeongjaeeom/concurrent/SpringBootConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jeongjaeeom.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConcurrentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
