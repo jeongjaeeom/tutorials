03c13d0f02a26b475863afbfe50aeae1532dd475
jeongjaeeom <jeongjae.eom@gmail.com>
2020-05-29 22:45:36 +0900

    CompletableFuture supplyAsync mehtod & executor 사용

--- spring-boot-concurrent/src/test/java/io/jeongjaeeom/concurrent/posts/component/PostFetchComponentTest.java
+++ spring-boot-concurrent/src/test/java/io/jeongjaeeom/concurrent/posts/component/PostFetchComponentTest.java
@@ -6,6 +6,7 @@ import io.jeongjaeeom.concurrent.posts.domain.Post;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +19,14 @@ class PostFetchComponentTest {
   private PostFetchComponent postFetchComponent;
 
   @Test
+  @DisplayName("포스트 조회 동기/블록킹 호출 테스트")
   void getPosts() {
     List<Post> posts = postFetchComponent.getPosts("재테크");
     assertThat(posts.size()).isEqualTo(4);
   }
 
   @Test
+  @DisplayName("포스트 조회 비동기/블록킹 호출 테스트")
   void getPostsAsyncByThread() {
     CompletableFuture<List<Post>> future = postFetchComponent.getPostsAsync("책");
     log.info("아직 최종 데이터를 전달 받지는 않았지만, 다른 작업 수행 가능");
