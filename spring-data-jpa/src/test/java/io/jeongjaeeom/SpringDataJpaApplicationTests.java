ff99ad38998503c2155c4e98e1f638bea356ba6c
jeongjaeeom <jeongjae.eom@gmail.com>
2020-06-25 12:32:27 +0900

    First Commit

---
+++ spring-data-jpa/src/test/java/io/jeongjaeeom/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jeongjaeeom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
